8554f9c9b083fff82ec914225edb28fdb4204fe7
Saffi <sksaffiulla786@gmail.com>
2024-01-18 11:18:48 +0530

    Almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
 import { useEffect, useState } from "react";
+import useQuoteInfo from "./Hooks/useQuoteInfo";
 
 const url = "https://api.quotable.io/random";
 
 function App() {
-  const [quote, setQuote] = useState("");
-  const [author, setAuthor] = useState("");
+  const [quote, setQuote] = useState({
+    content: "",
+    author: "",
+  });
 
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
-      .then((quote) => {
-        setQuote(quote.content);
-        setAuthor(quote.author);
+      .then((response) => {
+        setQuote({
+          ...quote,
+          content: response.content,
+          author: response.author,
+        });
       });
   }, []);
 
   const handleClick = () => {
     fetch(url)
       .then((response) => response.json())
-      .then((quote) => {
-        setQuote(quote.content);
-        setAuthor(quote.author);
+      .then((response) => {
+        setQuote({
+          ...quote,
+          content: response.content,
+          author: response.author,
+        });
       });
   };
 
   return (
     <div className="w-full h-screen flex justify-center items-center flex-col bg-slate-600">
-      <div className="w-1/3 rounded-xl shadow-lg p-2 bg-lime-200">
-        <p className="text-2xl font-serif text-center">{quote}</p>
+      <div className="md:w-1/3 w-3/4 rounded-xl shadow-lg p-2 bg-lime-200 ">
+        <p className="text-2xl font-serif text-center">{quote.content}</p>
         <p className="text-md font-serif font-bold italic text-center mt-1">
-          ~{author}~
+          ~{quote.author}~
         </p>
       </div>
 
